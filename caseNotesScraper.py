0e63d91560a6f389effcbe0fc1014ec579a39edc
Suz <2ricecake@gmail.com>
2021-04-14 11:38:45 +0900

    updated code

--- caseNotesScraper.py
+++ caseNotesScraper.py
@@ -1,61 +1,69 @@
 from bs4 import BeautifulSoup
 import requests
 import sys
+import time
 
 def save_2_txt(url):
     res = requests.get("https://casenote.kr"+url)
     html = res.text
     soup = BeautifulSoup(html, 'html.parser')
-    title = soup.select('#header > div')[1].getText(strip=True)
-    title_rm_space = title.replace(" ","")
-    content = soup.select('#mainbar')[0].getText()
-    # print(title_rm_space)
-    # print(content)
-    f = open("casenotes/"+title_rm_space+".txt", "w", encoding="utf-8")
-    f.write(str(content))
-    f.close()
-
-def save_2_html(url):
-    res = requests.get("https://casenote.kr"+url)
-    html = res.text
-    soup = BeautifulSoup(html, 'html.parser')
-    title = soup.select('#header > div')[1].getText(strip=True).replace(" ","")
-    content = soup.select('#mainbar')[0]
-    with open("casenotes/html/"+title+".html","w", encoding="utf-8") as f:
+    title = ""
+    print(title+"downloaded.")
+    try:
+        title = soup.select('#header > div')[1].getText(strip=True).replace(" ","")
+        content = soup.select('#mainbar')[0].getText()
+    except:
+        print("Could not save {0} as text.".format(url))
+    if len(title) > 0:
+        f = open(title+".txt", "w", encoding="utf-8")
         f.write(str(content))
-         
+        f.close()
 
-def crawler(maxPage, keyword, outputFormat):
-    page = 1 
-    
-    while page <= maxPage:
-        collection_url = "https://casenote.kr/search/?q="+keyword+"&p="+str(page)+"&sort=1&cc=3&ct=2" #ct for instance (2 is High Court, 3 Lower District Court)
+def opt_2_query(option_court, option_period, option_case):
+    court = "&court="+str(option_court)
+    period = "&period"+str(option_period)
+    case = "&case"+str(option_case)
+    opts = [court, period, case]
+    return opts
+
+def crawler(keyword, opts):
+    for page in range(20):
+        #NOTE: Using CaseNote's updated url format (2021.04.14)
+        collection_url = "https://casenote.kr/search/?q="+keyword+"&sort=0&"+opts[0]+opts[1]+opts[2]+"&page="+str(page) 
         col_res = requests.get(collection_url)
-        print("Status code:"+str(col_res.status_code))
-        col_html = col_res.text 
-        soup = BeautifulSoup(col_html, "html.parser")
-        for lnk in soup.select('.searched_item a'):
-            try: 
-                print("Retrieving Casenote...")
-                if outputFormat == "1":
+        if col_res.status_code == 200:
+            col_html = col_res.text
+            soup = BeautifulSoup(col_html, "html.parser")
+            for lnk in soup.select('.searched_item a'):
+                time.sleep(3)
+                try:
+                    print("Retrieving Casenote...")
                     save_2_txt(lnk['href'])
-                else: 
-                    save_2_html(lnk['href'])
-            except OSError as err:
-                print("OS error: {0}".format(err))
-                #NOTE: occurs when special chararcter is used in the title
-            except:
-                print("Unexpected error:", sys.exc_info()[0])
-                raise
-        page += 1
+                except OSError as err:
+                    print("OS error: {0}".format(err))
+                    #NOTE: occurs when special chararcter is used in the title
+                except:
+                    print("Unexpected error:", sys.exc_info()[0])
+                    raise
+        else:
+            print("Status code:"+str(col_res.status_code))
+        
 
 def main():
-    # keyword = sys.argv[1]
-    # maxPage = sys.argv[2]
     keyword = input("Keyword to search (키워드1+키워드2):")
-    maxPage = input("Number of Index Pages (1 page = 10 cases, max 20):") 
-    outputFormat = input("Output (html = 0, txt=1):")
-    crawler(int(maxPage),keyword,outputFormat)
 
-main()
+    print("대법원=0, 고등법원·특허법원=1, 지방법원·행정법원·가정법원=2, 헌법재판소=3")
+    option_court = input("Option[법원숫자&법원숫자]:")
+
+    print("전체=0, 최근 3년=1, 최근 5년=2, 최근 10년=3, 직접입력=4")
+    option_period = input("Option[기간숫자]:")
+
+    print("민사=0, 형사=1, 행정=2")
+    option_case = input("Option[사건종류&사건종류]:")
+
+    user_opts = opt_2_query(option_court, option_period, option_case)
+    crawler(keyword, user_opts)
+
+if __name__ == '__main__':
+    main()
 
